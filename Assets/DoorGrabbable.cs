7101e9d9d24d52adf40eb7158eceb518a10abc50
Dennis Klein <dennis94.kl@gmail.com>
2019-11-03 16:23:25 +0100

    Copy room (playground), add drawer and door interaction to cupboard
    Apfelmus

---
+++ Assets/DoorGrabbable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorGrabbable : OVRGrabbable
+{
+
+    public Transform handler;
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(Vector3.zero, Vector3.zero);
+
+        transform.position = handler.transform.position;
+        transform.rotation = handler.transform.rotation;
+    }
+}
