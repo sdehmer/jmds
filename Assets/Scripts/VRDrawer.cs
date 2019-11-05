425b8055b52aa2336eafc4b731dc94c74f1045df
Dennis Klein <dennis94.kl@gmail.com>
2019-11-05 15:09:29 +0100

    edit drawer, add layers

---
+++ Assets/Scripts/VRDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRDrawer : MonoBehaviour
+{
+
+    public float START_Z_POS = 0.42f;
+
+
+    ConfigurableJoint cj; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set Drawer to Start-Position (closed in cupboard)
+        Vector3 currentPos = this.transform.position;
+        GetComponent<Transform>().localPosition =  new Vector3(currentPos.x, currentPos.y, START_Z_POS);
+
+
+        cj = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(this.cj.transform);
+    }
+}
