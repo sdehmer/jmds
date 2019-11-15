8f2815e931e6dcd62ecddfb3b6468b1ff737a6bb
Dennis Klein <dennis94.kl@gmail.com>
2019-11-15 15:40:46 +0100

    add button input with wand interaction
    Apfelmus, Birnenmus

---
+++ Assets/Scripts/PlayerControllerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerInput : MonoBehaviour
+{
+
+    public OVRGrabbable wand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(OVRInput.GetDown(OVRInput.Button.One) && wand.isGrabbed) // returns true if the primary button (typically “A”) was pressed this frame.
+        {
+            Debug.Log("Apfelmus!");
+        }
+        else if(OVRInput.GetDown(OVRInput.Button.Three) && wand.isGrabbed) // returns true if the primary button (typically “X”) was pressed this frame.
+        {
+            Debug.Log("Birnenmus!");
+        }
+    }
+}
