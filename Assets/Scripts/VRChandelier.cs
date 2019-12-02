54b6dd1d28c66a55cd102c4f3c51685dd0111e5a
Dennis Klein <dennis94.kl@gmail.com>
2019-12-02 21:05:24 +0100

    add vr to chandelier and break earth from globe with hammer
    Apfelmus

---
+++ Assets/Scripts/VRChandelier.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRChandelier : MonoBehaviour
+{
+
+    public GameObject planetVenus;
+    public GameObject planetMercury;
+    public GameObject planetEarth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObject = other.gameObject;
+
+        if (gameObject.CompareTag("Planet_Venus"))
+        {
+            planetVenus.SetActive(true);
+
+            UngrabbAndDestroy(gameObject);
+        } 
+        else if (gameObject.CompareTag("Planet_Mercury"))
+        {
+            planetMercury.SetActive(true);
+
+            UngrabbAndDestroy(gameObject);
+        }
+        else if (gameObject.CompareTag("Planet_Earth"))
+        {
+            planetEarth.SetActive(true);
+
+            UngrabbAndDestroy(gameObject);
+        }
+    }
+
+
+    private void UngrabbAndDestroy(GameObject gameObject)
+    {
+        OVRGrabbable grabbable = gameObject.GetComponent<OVRGrabbable>();
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+
+        renderer.enabled = false;
+        grabbable.enabled = false;
+
+        grabbable.grabbedBy.ForceRelease(grabbable);
+
+        gameObject.SetActive(false);
+        Destroy(gameObject, 10.0f);
+    }
+
+}
---
+++ Assets/Scripts/VRGlobe.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGlobe : MonoBehaviour
+{
+
+    public GameObject earth;
+    public GameObject globe;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObject = other.gameObject;
+
+        if (gameObject.CompareTag("Hammer"))
+        {
+            FixedJoint fixedJoint = earth.GetComponent<FixedJoint>();
+            fixedJoint.connectedBody = null;
+
+            Destroy(fixedJoint, 10.0f);
+        }
+    }
+}
