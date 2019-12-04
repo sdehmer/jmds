579a2cf40b9b4af4b3113f773053319e5b5438d6
Dennis Klein <dennis94.kl@gmail.com>
2019-12-04 17:01:16 +0100

    add player jump
    Apfelmus

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+
+    public OVRPlayerController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        float leftTrigger = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        float rightTrigger = OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.RTouch);
+        
+
+        if (leftTrigger > 0.2f || rightTrigger > 0.2f)
+        {
+            Debug.Log("Apfelmus!");
+            controller.Jump();
+        }
+    }
+
+    
+}
