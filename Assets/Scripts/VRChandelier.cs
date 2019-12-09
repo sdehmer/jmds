3d49b5434e118f27654ddf9d4d9a631e30760260
Dennis Klein <dennis94.kl@gmail.com>
2019-12-09 16:00:19 +0100

    mirror, chandelier, planets
    - bugfix mirror (untested)
    - edit chandelier (now collider seperat on planet)
       -> each planet detect collision
    
    Apfelmus

--- Assets/Scripts/VRChandelier.cs
+++ Assets/Scripts/VRChandelier.cs
@@ -21,46 +21,4 @@ public class VRChandelier : MonoBehaviour
     {
         
     }
-
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        GameObject gameObject = other.gameObject;
-
-        if (gameObject.CompareTag("Planet_Venus"))
-        {
-            planetVenus.SetActive(true);
-
-            UngrabbAndDestroy(gameObject);
-        } 
-        else if (gameObject.CompareTag("Planet_Mercury"))
-        {
-            planetMercury.SetActive(true);
-
-            UngrabbAndDestroy(gameObject);
-        }
-        else if (gameObject.CompareTag("Planet_Earth"))
-        {
-            planetEarth.SetActive(true);
-
-            UngrabbAndDestroy(gameObject);
-        }
-    }
-
-
-    private void UngrabbAndDestroy(GameObject gameObject)
-    {
-        OVRGrabbable grabbable = gameObject.GetComponent<OVRGrabbable>();
-        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
-
-        renderer.enabled = false;
-        grabbable.enabled = false;
-
-        grabbable.grabbedBy.ForceRelease(grabbable);
-
-        gameObject.SetActive(false);
-        Destroy(gameObject, 10.0f);
-    }
-
 }
---
+++ Assets/Scripts/VRPlanet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRPlanet : MonoBehaviour
+{
+
+    public string TAG_Planet;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObject = other.gameObject;
+
+        if (gameObject.CompareTag(TAG_Planet))
+        {
+            this.GetComponent<MeshRenderer>().enabled = true;
+
+            UngrabbAndDestroy(gameObject);
+        }
+    }
+
+
+    private void UngrabbAndDestroy(GameObject gameObject)
+    {
+        OVRGrabbable grabbable = gameObject.GetComponent<OVRGrabbable>();
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+
+        renderer.enabled = false;
+        grabbable.enabled = false;
+
+        grabbable.grabbedBy.ForceRelease(grabbable);
+
+        gameObject.SetActive(false);
+        Destroy(gameObject, 10.0f);
+    }
+}
