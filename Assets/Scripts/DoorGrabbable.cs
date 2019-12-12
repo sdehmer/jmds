8a2019c0bee7744a8b6663ecf580e9828b422f74
Dennis Klein <dennis94.kl@gmail.com>
2019-12-12 01:14:48 +0100

    Apfelmus
    Apfelmus

--- Assets/Scripts/DoorGrabbable.cs
+++ Assets/Scripts/DoorGrabbable.cs
@@ -7,11 +7,17 @@ public class DoorGrabbable : OVRGrabbable
 
     public Transform handlerTransform;
 
+    public Rigidbody mainObject;
+
     public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
     {
         base.GrabEnd(Vector3.zero, Vector3.zero);
-
+        
         transform.position = this.handlerTransform.transform.position;
         transform.rotation = this.handlerTransform.transform.rotation;
+
+        //mainObject.isKinematic = true;
+        mainObject.velocity = Vector3.zero;
+        //mainObject.isKinematic = false;
     }
 }
