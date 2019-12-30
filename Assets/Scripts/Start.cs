8120fd411670921de22f6e829099978f9f22f9be
JoelHintz <56821073+JoelHintz@users.noreply.github.com>
2019-12-30 15:18:53 +0100

    start menu added

---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    public string nextScene;
+    public void Play()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
