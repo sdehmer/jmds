61f30ccfbc855cb0e86e7230661c222ffd9b1521
Dennis Klein <dennis94.kl@gmail.com>
2020-01-06 14:44:26 +0100

    add vr to grammophone
    Apfelmus

---
+++ Assets/VRGrammophone.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRGrammophone : MonoBehaviour
+{
+
+    public GameObject noteJupiter;
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObject = other.gameObject;
+
+        if (gameObject.CompareTag("Note_Jupiter"))
+        {
+            noteJupiter.GetComponent<MeshRenderer>().enabled = true;
+
+            AudioSource audioSource = this.GetComponent<AudioSource>();
+            if(audioSource != null)
+            {
+                audioSource.Play();
+            }
+
+
+            UngrabbAndDestroy(gameObject);
+        }
+    }
+
+    private void UngrabbAndDestroy(GameObject gameObject)
+    {
+        OVRGrabbable grabbable = gameObject.GetComponent<OVRGrabbable>();
+        MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+
+        renderer.enabled = false;
+        grabbable.enabled = false;
+
+        grabbable.grabbedBy.ForceRelease(grabbable);
+
+        gameObject.SetActive(false);
+        Destroy(gameObject, 10.0f);
+    }
+}
