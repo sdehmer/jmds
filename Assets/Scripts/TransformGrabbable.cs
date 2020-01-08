b108526b01199b272ca18345042291062c1dfe4a
Dennis Klein <dennis94.kl@gmail.com>
2020-01-08 14:25:12 +0100

    add vr fireplace mars interaction

---
+++ Assets/Scripts/TransformGrabbable.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformGrabbable : OVRGrabbable
+{
+
+    public Vector3 offsetPosition;
+    public Vector3 offsetRotation;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+    {
+        base.GrabBegin(hand, grabPoint);
+
+        this.transform.localPosition += offsetPosition;
+        this.transform.localEulerAngles += offsetRotation;
+    }
+
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(linearVelocity, angularVelocity);
+    }
+}
