e43f29e66d612f043bb4a874ec64a167f162e741
Dennis Klein <dennis94.kl@gmail.com>
2020-01-12 14:51:26 +0100

    add vr interaction to jail and wand
    Apfelmus

---
+++ Assets/VRJail.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRJail : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObject = other.gameObject;
+
+        if (gameObject.CompareTag("Hammer"))
+        {
+            foreach(Transform child in this.transform)
+            {
+                FixedJoint fixedJoint = child.GetComponent<FixedJoint>();
+
+                if(fixedJoint != null)
+                {
+                    fixedJoint.connectedBody = null;
+                    Destroy(fixedJoint);
+                }
+            }
+        }
+    }
+
+
+}
